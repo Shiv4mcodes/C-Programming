d4ca682996cc992bb260d98e654de9f83f24c6d9
Shivam <84087343+Shiv4mcodes@users.noreply.github.com>
2026-07-06 00:07:19 +0530

    Ternary

---
+++ Chapter3/05_ternary.c
@@ -0,0 +1,11 @@
+//a short if - else cann be written using conditionals and ternary opertators
+
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    int b = 3;
+
+    a>b? printf("A is greater"):printf("B is greater");
+    return 0;
+}
