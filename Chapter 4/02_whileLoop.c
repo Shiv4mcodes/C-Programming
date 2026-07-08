ca10205ef590b4c0b3823f56610ad5531443a6ef
Shivam <84087343+Shiv4mcodes@users.noreply.github.com>
2026-07-08 22:57:00 +0530

    while loops

---
+++ Chapter 4/02_whileLoop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int i = 0;                                  // initialization of i = 0
+    while (i < 1000)                              // while loop condition is true 
+    {
+        printf("Happy Birthday Shubhang \n");            // loop body 
+        i++;                                         // i = i + 1; if not written this line the loop will run infinitely
+    }
+    
+    return 0;
+}
+
+
