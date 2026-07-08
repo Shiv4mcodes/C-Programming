330636634a70d896c86171b0545dff7d6d576b81
Shivam <84087343+Shiv4mcodes@users.noreply.github.com>
2026-07-08 22:57:37 +0530

    Infinity LOOPS

---
+++ Chapter 4/03_infinityLoop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int i = 0;                                  // initialization of i = 0
+    while (i < 4)                              // while loop condition is true 
+    {
+        printf("Happy Birthday Rage. \n");    // loop body 
+                                               // i = i + 1; if not written this line the loop will run infinitely
+    }
+    
+    return 0;
+}
+
+
