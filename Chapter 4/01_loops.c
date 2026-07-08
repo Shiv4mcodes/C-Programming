84bf92d21a41c4db2a25da08c8e671d95f9b29ed
Shivam <84087343+Shiv4mcodes@users.noreply.github.com>
2026-07-08 22:54:13 +0530

    Loops

---
+++ Chapter 4/01_loops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//print 1lakh times. Here comes loop concept 
+int main(){
+    printf("Happy birthday Rage\n");
+    printf("Happy birthday Rage\n");
+    printf("Happy birthday Rage\n");
+    printf("Happy birthday Rage\n");
+    printf("Happy birthday Rage\n");
+    printf("Happy birthday Rage\n");
+    printf("Happy birthday Rage\n");
+    printf("Happy birthday Rage\n");
+    printf("Happy birthday Rage\n");
+    return 0;
+}
