f81c37f6f0a5b7294ac588858d3a3cf8e46f5c42
Shivam Mishra <84087343+Shiv4mcodes@users.noreply.github.com>
2026-07-09 23:40:13 +0530

    Problem-2

---
+++ Chapter 4/Problem/02_problem.c
@@ -0,0 +1,15 @@
+// multiplication of the number but in reversed order
+
+#include <stdio.h>
+
+int main(){
+    int n ;
+    printf("Enter the number: ");
+    scanf("%d" , &n);
+
+    for (int i = 10; i ; i--)
+    {
+        printf("%d X %d = %d\n" , n , i , n*i);
+    }
+    return 0;
+}
