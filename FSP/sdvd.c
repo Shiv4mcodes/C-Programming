160b50af19c18d1a7acf2ff37e417f0e55c3206e
Shivam <84087343+Shiv4mcodes@users.noreply.github.com>
2026-07-10 22:34:38 +0530

    5x5 matrix

---
+++ FSP/sdvd.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // Define the 5x5 matrix
+    int matrix[5][5] = {
+        {17, 24, 1, 8, 15},
+        {23, 5, 7, 14, 16},
+        {4, 6, 13, 20, 22},
+        {10, 12, 19, 21, 3},
+        {11, 18, 25, 2, 9}
+       
+    };
+
+    // Print the matrix
+    printf("The 5x5 matrix is:\n");
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+
+#include <stdio.h>
+
+int main(){
+    printf("\n\n\n ")
+    return 0;
+}
